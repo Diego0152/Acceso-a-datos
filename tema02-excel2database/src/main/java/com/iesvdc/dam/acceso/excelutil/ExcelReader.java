9df693006b8c5bd9883fc9209c51a5f122599d2d
Diego Ramos <dramrub2510@g.educaand.es>
2025-10-30 09:57:29 +0100

    Modelo Entrega 2.0.0

--- tema02-excel2database/src/main/java/com/iesvdc/dam/acceso/excelutil/ExcelReader.java
+++ tema02-excel2database/src/main/java/com/iesvdc/dam/acceso/excelutil/ExcelReader.java
@@ -3,6 +3,7 @@ package com.iesvdc.dam.acceso.excelutil;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Properties;
 
 import org.apache.poi.ss.usermodel.Sheet;
@@ -18,7 +19,7 @@ public class ExcelReader {
     
     public static XSSFWorkbook loadExcel(Connection conexion) {
         Properties prop = Config.getProperties("config.properties");
-        
+
         String inFile = prop.getProperty("inputFile");
         XSSFWorkbook wb = null;
 
@@ -33,15 +34,26 @@ public class ExcelReader {
     
     public static void loadToDataBase(Connection conexion, XSSFWorkbook wb) {
         final double EPSILON = 1e-10;
-        PreparedStatement pStat = null;
-        StringBuilder sentSQLCRTB = new StringBuilder();
-        StringBuilder sentSQLINVL = new StringBuilder();
+
         int numHojas = wb.getNumberOfSheets();
 
+        StringBuilder sentSQLCRTB = new StringBuilder();
+        StringBuilder sentSQLINVL = new StringBuilder();
+
         try {
+
+            Statement pStatIN = conexion.createStatement();
+            Statement pStatCT = conexion.createStatement();
+
             for (int i = 0; i < numHojas; i++) {
                 Sheet hoja = wb.getSheetAt(i);
 
+                if (sentSQLCRTB.length() != 0) 
+                    sentSQLCRTB.setLength(0);
+                
+                if (sentSQLINVL.length() != 0) 
+                    sentSQLINVL.setLength(0);
+                
                 sentSQLCRTB.append("CREATE TABLE " + hoja.getSheetName() + " (\n");
                 sentSQLINVL.append("INSERT INTO " + hoja.getSheetName() + " (");
 
@@ -117,12 +129,12 @@ public class ExcelReader {
 
                 sentSQLINVL.append("VALUES (");
 
-                for (int k = 1; k < numTablas; k++) {
+                for (int k = 1; k <= numTablas; k++) {
                     Row fila = hoja.getRow(k);
                     int numCeldas = fila.getLastCellNum();
                     
                     for (int l = 0; l < numCeldas; l++) {
-                        sentSQLINVL.append(fila.getCell(l));
+                        sentSQLINVL.append("'" + fila.getCell(l) + "'");
 
                         if (l < numColumnas - 1) {
                             sentSQLINVL.append(", ");
@@ -131,10 +143,9 @@ public class ExcelReader {
                         }
                         
                     } 
-                 
                     
                     sentSQLINVL.append(")");
-                    if (k < numTablas - 1) {
+                    if (k <= numTablas - 1) {
                         sentSQLINVL.append(",\n(");
                     } else {
                         sentSQLINVL.append(";\n");
@@ -142,20 +153,33 @@ public class ExcelReader {
                 }
 
                 sentSQLCRTB.append(") ENGINE=InnoDB;");
-                pStat = conexion.prepareStatement(sentSQLCRTB.toString());
-                if (pStat.executeUpdate() == 0) {
-                    System.out.println("Tabla " + hoja.getSheetName() + " creada.");
+                int verifCT = pStatCT.executeUpdate(sentSQLCRTB.toString());
+                if (verifCT == 0) {
+                    System.out.println("\nTabla " + hoja.getSheetName() + " creada.\n");
                     System.out.println(sentSQLCRTB.toString());
-                    System.out.println(sentSQLINVL.toString());
+                    System.out.println();
+                    
+                    
                 } else {
                     System.err.println("Error al crear la tabla.");
                 }
-                System.out.println(sentSQLINVL.toString());
-
+                
+                int verifIN = pStatIN.executeUpdate(sentSQLINVL.toString());
+                
+                if (verifIN > 0) {
+                    System.out.println("Datos insertados de: " + hoja.getSheetName() + "\n");
+                    System.out.println(sentSQLINVL.toString());
+                    System.out.println();
+                    
+                } else {
+                    System.err.println("Error al crear la tabla.");
+                }
+                
             }
             
         } catch (SQLException e) {
-            System.err.println("Error: No se ha podido meter los datos a la BBDD.");
+            System.err.println("Error: Ha habido un problema en el método loadToDataBase.");
+            e.printStackTrace();
         }
         
     }
